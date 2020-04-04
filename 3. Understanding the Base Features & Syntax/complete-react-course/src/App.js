eb7c1a7013322ed9836a18dfac065eddb8d2c8dd
Michael <i388177@fhict.nl>
2020-04-04 14:02:36 +0200

    36. Working with Props

--- 3. Understanding the Base Features & Syntax/complete-react-course/src/App.js
+++ 3. Understanding the Base Features & Syntax/complete-react-course/src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React app!</h1>
         <p>This is really working.</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Mike" age="24"/>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="26">My hobbies: Racing</Person>
       </div>
     );
 
