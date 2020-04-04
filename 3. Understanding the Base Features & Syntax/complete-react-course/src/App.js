843bc783a5c2b4a537744bd1df03c72629dc7cc6
Michael <i388177@fhict.nl>
2020-04-04 13:44:06 +0200

    31. JSX Restrictions

--- 3. Understanding the Base Features & Syntax/complete-react-course/src/App.js
+++ 3. Understanding the Base Features & Syntax/complete-react-course/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React app!</h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React app!</h1>
+        <p>This is really working.</p>
+      </div>
+    );
 
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
