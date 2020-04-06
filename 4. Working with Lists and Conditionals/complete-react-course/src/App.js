84502fe2ea02b845b7b52cee4aabf96424dd00b8
Michael <i388177@fhict.nl>
2020-04-06 23:40:05 +0200

    58. Updating State Immutably

--- 4. Working with Lists and Conditionals/complete-react-course/src/App.js
+++ 4. Working with Lists and Conditionals/complete-react-course/src/App.js
@@ -25,10 +25,10 @@ class App extends Component {
   };
 
   deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.splice();
     const persons = this.state.persons;
     persons.splice(personIndex, 1);
     this.setState({persons: persons});
-    console.log(persons);
   }
 
   togglePersonsNameHandler = () => {
